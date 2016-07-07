8fa915217ef0d62baca7716dcb0e45b7afef19f2
s4ntia60 <sapalacio2@gmail.com>
2016-07-07 14:09:22 +0200

    Visual Studio Git Hub plugin test

--- MyRayTracer/Transform.h
+++ MyRayTracer/Transform.h
@@ -12,10 +12,6 @@ using namespace glm;
 // glm provides vector, matrix classes like glsl
 // Typedefs to make code more readable 
 
-//typedef glm::mat3 mat3 ;
-//typedef glm::mat4 mat4 ; 
-//typedef glm::vec3 vec3 ; 
-//typedef glm::vec4 vec4 ; 
 const float pi = 3.14159265f ; // For portability across platforms
 
 
